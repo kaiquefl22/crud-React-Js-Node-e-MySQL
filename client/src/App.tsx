f0664f20c6d4bf80d8db7582d84f8b1c96aab69e
kaiquefl22@hotmail.com <kaiquefl22@hotmail.com>
2022-06-23 13:44:44 -0300

    cliente

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app--container">
+      <div className="register--container">
+        <h1 className='register--title'>Scrim Shop</h1>
+        <input type="text" name='name' placeholder='Nome' className='register--input' />
+        <input type="text" name='cost' placeholder='Preço' className='register--input' />
+        <input type="text" name='category' placeholder='Categoria' className='register--input' />
+        <button className='register--button'>Cadastrar</button>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
